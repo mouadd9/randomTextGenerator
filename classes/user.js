7292d07fd09350a46e699d96cc790dfe41458d76
mouadispunk <bencaidmouad9@gmail.com>
2024-06-02 15:39:11 +0100

    made an initial design for the form and a class for users

---
+++ classes/user.js
@@ -0,0 +1,11 @@
+
+
+
+class user {
+    constructor(name, surname, cat){
+        this._name = name;
+        this._surname = surname;
+        this._cat = cat;
+    }
+    
+}
---
+++ modules/stringsDataModule.js
@@ -0,0 +1,5 @@
+// here we should have a data structure that should hold the strings we will use 
+// a function that will extract a random string string
+// a function that will add a string to the array
+
+let array = [""];
